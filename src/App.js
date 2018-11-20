6f41ccf9e3490be52774678bb77a234963dba6fc
bodhicougar <contact@blackboxmd.fyi>
2018-11-20 09:20:19 +0100

    third commit: eingedeutscht.
    Signed-off-by: bodhicougar <contact@blackboxmd.fyi>

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Editiere <code>src/App.js</code> & speichere um neu anzuzeigen.
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Lerne React aber rasch!
           </a>
         </header>
       </div>
